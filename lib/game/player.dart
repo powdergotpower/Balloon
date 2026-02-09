c530178c0e26533ee7d25aded1bd46b938c89c2d
powdergotpower <anshjotsinghsidhu@gmail.com>
2026-02-09 08:40:27 +0530

    Remove unused import from player.dart
    Removed unused import for flame/events.dart.

--- lib/game/player.dart
+++ lib/game/player.dart
@@ -1,7 +1,6 @@
 import 'dart:math';
 
 import 'package:flame/components.dart';
-import 'package:flame/events.dart';
 import 'package:flame/collisions.dart';
 import 'package:flutter/material.dart';
 
