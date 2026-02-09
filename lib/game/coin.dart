00675dea82f27590a7ec97e3d7f1d9cbb13bad15
powdergotpower <anshjotsinghsidhu@gmail.com>
2026-02-09 08:42:06 +0530

    Update coin.dart

--- lib/game/coin.dart
+++ lib/game/coin.dart
@@ -1,41 +1,57 @@
 import 'package:flame/components.dart';
 import 'package:flame/collisions.dart';
 import 'package:flutter/material.dart';
-import 'player.dart';
+
+import 'balloon_game.dart';     // ← import the game class
+import 'player.dart';           // for PlayerBalloon type
 
 class Coin extends CircleComponent
-    with CollisionCallbacks, HasGameRef {
+    with CollisionCallbacks, HasGameRef<BalloonGame> {
   final VoidCallback onCollect;
 
-  Coin({required Vector2 position, required this.onCollect})
-      : super(
+  Coin({
+    required Vector2 position,
+    required this.onCollect,
+  }) : super(
           position: position,
-          radius: 6,
-          paint: Paint()..color = Colors.grey,
+          radius: 12,                    // bigger = easier to see/collect
+          anchor: Anchor.center,
+          paint: Paint()..color = Colors.amber,
         );
 
   @override
   Future<void> onLoad() async {
-    add(CircleHitbox());
+    super.onLoad();
+
+    // Add hitbox for collision detection
+    add(
+      CircleHitbox()
+        ..collisionType = CollisionType.passive  // coins don't push back
+        ..isSolid = false,
+    );
   }
 
   @override
   void update(double dt) {
-    super.update(dt);
+    super.update(dt);           // ← IMPORTANT: fixes must_call_super warning
+
+    // Fall down
     position.y += 120 * dt;
 
-    if (position.y > gameRef.size.y + 20) {
+    // Remove when off screen
+    if (position.y > gameRef.size.y + 50) {
       removeFromParent();
     }
   }
 
+  // Using onCollision (simpler & matches your player code)
   @override
-  void onCollisionStart(
-      Set<Vector2> intersectionPoints, PositionComponent other) {
+  void onCollision(Set<Vector2> intersectionPoints, PositionComponent other) {
+    super.onCollision(intersectionPoints, other);
+
     if (other is PlayerBalloon) {
-      onCollect();
-      removeFromParent();
+      onCollect();             // increase score
+      removeFromParent();      // disappear
     }
-    super.onCollisionStart(intersectionPoints, other);
   }
 }
